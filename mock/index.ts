5b33fbad970d3c84ede63896971accb5015f84a4
liuxiaowei <lxwyouxiang1994@163.com>
2021-09-07 17:39:02 +0800

    bug

--- mock/index.ts
+++ mock/index.ts
@@ -2,6 +2,29 @@ import { MockMethod } from 'vite-plugin-mock'
 
 
 export default [
+  /**
+   * 练习
+   * @return { name -> 练习名称  }
+   */
+   {
+    url: "/api/practice/getPractice",
+    method: "get",
+    response: ({}) => {
+      return {
+        success: true,
+        message: null,
+        data: {
+          id: "@uid",
+          practiceType: "专项练习",
+          userId: "@guid",
+          name: "@cname",
+          createTime: "@datetime",
+          courseId: "@guid",
+          complete: false,
+        },
+      };
+    },
+  },
     {
       url: '/api/get',
       method: 'get',
