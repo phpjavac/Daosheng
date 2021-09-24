267f431944b7393f97c387ad54636e1a9a9ba5f7
yinzhenyu-su <sharkrain@outlook.com>
2021-09-24 12:11:34 +0800

    fix(TS-1071): 🐛 修复husky脚本不生效的问题

--- commitlint.config.js
+++ commitlint.config.js
@@ -21,6 +21,7 @@ const $$types = [
 ];
 // https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
 // https://github.com/vidavidorra/commitlint-plugin-function-rules#usage
+// https://github.com/folke/devmoji#default-devmoji-reference
 // 使用 function rules 时，要先禁用同名的rules，否则会重复校验
 module.exports = {
   extends: ["@commitlint/config-conventional"],
@@ -36,6 +37,24 @@ module.exports = {
           return [true];
         }
         return [false, "提交信息的长度不能超过72个字符"];
+      },
+    ],
+    "scope-enum": [0],
+    "function-rules/scope-enum": [
+      2,
+      "always",
+      (parsed) => {
+        const regScope = new RegExp(/[a-zA-Z]{2,20}-[0-9]{1,20}/, "gm");
+        if (parsed.scope) {
+          if (regScope.test(parsed.scope)) {
+            return [true];
+          }
+          return [false, "jira 任务号不正确"];
+        }
+        return [
+          false,
+          "请填写 jira 任务号 \neg:\n fix(DCAW-900): 修复跳转链接",
+        ];
       },
     ],
   },
