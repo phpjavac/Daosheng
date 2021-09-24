eb74e4b13339824f92eb10c08324d48133a64406
yinzhenyu-su <sharkrain@outlook.com>
2021-09-24 13:23:52 +0800

    feat(#5): 添加commitlint支持

--- commitlint.config.js
+++ commitlint.config.js
@@ -44,17 +44,16 @@ module.exports = {
       2,
       "always",
       (parsed) => {
-        const regScope = new RegExp(/[a-zA-Z]{2,20}-[0-9]{1,20}/, "gm");
+        const helpMsg = `请填写 jira 任务号 或 issue 号 \neg:\n fix(DCAW-900): 修复跳转链接\n fix(#123): 修复跳转链接\n`;
+        const jiraScope = new RegExp(/[a-zA-Z]{2,20}-[0-9]{1,20}/, "g");
+        const issueScope = new RegExp(/#[0-9]{1,20}/, "g");
         if (parsed.scope) {
-          if (regScope.test(parsed.scope)) {
+          if (jiraScope.test(parsed.scope) || issueScope.test(parsed.scope)) {
             return [true];
           }
-          return [false, "jira 任务号不正确"];
+          return [false, helpMsg];
         }
-        return [
-          false,
-          "请填写 jira 任务号 \neg:\n fix(DCAW-900): 修复跳转链接",
-        ];
+        return [false, helpMsg];
       },
     ],
   },
